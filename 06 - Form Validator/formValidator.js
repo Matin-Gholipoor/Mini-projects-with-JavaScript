e205d9cc74f9d367ae503047165e5a76c66cf7db
Matin-Gholipoor <matin.3108@gmail.com>
2025-08-23 18:21:18 +0330

    '06 - Form Validator' - field correctness & submit action implemented. project finished

--- 06 - Form Validator/formValidator.js
+++ 06 - Form Validator/formValidator.js
@@ -12,6 +12,24 @@ function submit(event) {
 	const passwordError = passwordInput.nextElementSibling;
 	const confirmPasswordError = confirmPasswordInput.nextElementSibling;
 
+	let isUsernameCorrect = true;
+	let isEmailCorrect = true;
+	let isPasswordCorrect = true;
+	let isConfirmPasswordCorrect = true;
+
+	usernameInput.classList.add('input-correct');
+	usernameInput.classList.add('input-correct:focus');
+	usernameError.textContent = '';
+	emailInput.classList.add('input-correct');
+	emailInput.classList.add('input-correct:focus');
+	emailError.textContent = '';
+	passwordInput.classList.add('input-correct');
+	passwordInput.classList.add('input-correct:focus');
+	passwordError.textContent = '';
+	confirmPasswordInput.classList.add('input-correct');
+	confirmPasswordInput.classList.add('input-correct:focus');
+	confirmPasswordError.textContent = '';
+
 	if (!usernameInput.value) {
 		usernameError.textContent = 'Username is required.';
 
@@ -19,6 +37,12 @@ function submit(event) {
 			usernameInput.classList.add('input-error');
 			usernameInput.classList.add('input-error:focus');
 		}
+		if (usernameInput.classList.contains('input-correct')) {
+			usernameInput.classList.remove('input-correct');
+			usernameInput.classList.remove('input-correct:focus');
+		}
+
+		isUsernameCorrect = false;
 	}
 	else if (usernameInput.value.length < 3) {
 		usernameError.textContent = 'Username must be at least 3 characters.';
@@ -27,6 +51,12 @@ function submit(event) {
 			usernameInput.classList.add('input-error');
 			usernameInput.classList.add('input-error:focus');
 		}
+		if (usernameInput.classList.contains('input-correct')) {
+			usernameInput.classList.remove('input-correct');
+			usernameInput.classList.remove('input-correct:focus');
+		}
+
+		isUsernameCorrect = false;
 	}
 
 	if (!emailInput.value) {
@@ -36,6 +66,12 @@ function submit(event) {
 			emailInput.classList.add('input-error');
 			emailInput.classList.add('input-error:focus');
 		}
+		if (emailInput.classList.contains('input-correct')) {
+			emailInput.classList.remove('input-correct');
+			emailInput.classList.remove('input-correct:focus');
+		}
+
+		isEmailCorrect = false;
 	}
 	else if (!emailInput.checkValidity()) {
 		emailError.textContent = 'Email is not valid.';
@@ -44,6 +80,12 @@ function submit(event) {
 			emailInput.classList.add('input-error');
 			emailInput.classList.add('input-error:focus');
 		}
+		if (emailInput.classList.contains('input-correct')) {
+			emailInput.classList.remove('input-correct');
+			emailInput.classList.remove('input-correct:focus');
+		}
+
+		isEmailCorrect = false;
 	}
 
 	if (!passwordInput.value) {
@@ -53,6 +95,12 @@ function submit(event) {
 			passwordInput.classList.add('input-error');
 			passwordInput.classList.add('input-error:focus');
 		}
+		if (passwordInput.classList.contains('input-correct')) {
+			passwordInput.classList.remove('input-correct');
+			passwordInput.classList.remove('input-correct:focus');
+		}
+
+		isPasswordCorrect = false;
 	}
 	else if (passwordInput.value.length < 6) {
 		passwordError.textContent = 'Password must be at least 6 characters.';
@@ -61,6 +109,12 @@ function submit(event) {
 			passwordInput.classList.add('input-error');
 			passwordInput.classList.add('input-error:focus');
 		}
+		if (passwordInput.classList.contains('input-correct')) {
+			passwordInput.classList.remove('input-correct');
+			passwordInput.classList.remove('input-correct:focus');
+		}
+
+		isPasswordCorrect = false;
 	}
 
 	if (!confirmPasswordInput.value) {
@@ -70,6 +124,12 @@ function submit(event) {
 			confirmPasswordInput.classList.add('input-error');
 			confirmPasswordInput.classList.add('input-error:focus');
 		}
+		if (confirmPasswordInput.classList.contains('input-correct')) {
+			confirmPasswordInput.classList.remove('input-correct');
+			confirmPasswordInput.classList.remove('input-correct:focus');
+		}
+
+		isConfirmPasswordCorrect = false;
 	}
 
 	if (passwordInput.value && confirmPasswordInput.value) {
@@ -80,6 +140,22 @@ function submit(event) {
 				confirmPasswordInput.classList.add('input-error');
 				confirmPasswordInput.classList.add('input-error:focus');
 			}
+			if (confirmPasswordInput.classList.contains('input-correct')) {
+				confirmPasswordInput.classList.remove('input-correct');
+				confirmPasswordInput.classList.remove('input-correct:focus');
+			}
+
+			isConfirmPasswordCorrect = false;
 		}
+	}
+
+	if (
+		isUsernameCorrect &&
+		isEmailCorrect &&
+		isPasswordCorrect &&
+		isConfirmPasswordCorrect
+	) {
+		window.alert('Registration successful!');
+		location.reload();
 	}
 }
