071b013fa0b73ed1f5f7e8a349a54f115c3a9b9c
Matin-Gholipoor <matin.3108@gmail.com>
2025-08-31 09:03:20 +0330

    '09 - Recipe Finder' - search implemented

---
+++ 09 - Recipe Finder/recipeFinder.js
@@ -0,0 +1,75 @@
+const searchInput = document.querySelector('.js-search-input');
+const searchButton = document.querySelector('.js-search-button');
+const searchingMessage = document.querySelector('.js-searching-message');
+const errorMessage = document.querySelector('.js-error-message');
+const resultsContainer = document.querySelector('.js-results-container');
+const backButton = document.querySelector('.js-back-button');
+const recipeSection = document.querySelector('.js-recipe-section');
+const recipeSectionTitle = document.querySelector('.js-recipe-section-title');
+const recipeSectionTag = document.querySelector('.js-recipe-section-tag');
+const instructionText = document.querySelector('.js-instructions-text');
+const ingredientsSection = document.querySelector('.js-ingredients-section');
+
+const BASE_URL = 'https://www.themealdb.com/api/json/v1/1/';
+const SEARCH_URL = `${BASE_URL}search.php?s=`;
+const LOOKUP_URL = `${BASE_URL}lookup.php?i=`;
+
+searchButton.addEventListener('click', search);
+searchInput.addEventListener('keypress', (event) => {
+  if (event.key === 'Enter')
+    search();
+});
+
+function search() {
+  const searchKeyword = searchInput.value.replace(/ /g, "_");
+  searchInput.value = '';
+
+  searchingMessage.style.display = 'block';
+  searchingMessage.textContent = `Searching for "${searchKeyword}"...`;
+
+  errorMessage.style.display = 'none';
+
+  resultsContainer.style.display = 'none';
+
+  fetch(`${SEARCH_URL}${searchKeyword}`)
+    .then(response => {
+      if (response.ok)
+        return response.json();
+      else
+        throw new Error('something went wrong while searching.');
+    })
+    .then(data => {
+      const foods = data.meals;
+
+      searchingMessage.textContent = `Search results for "${searchKeyword}":`;
+
+      if (foods) {
+        resultsContainer.style.display = 'grid';
+        resultsContainer.innerHTML = '';
+        foods.forEach((food) => {
+          resultsContainer.innerHTML += `
+          <div class="food-card">
+            <img src=${food.strMealThumb} class="food-thumbnail">
+            <div class="result-card-bottom-row">
+              <p class="food-name">
+                ${food.strMeal}
+              </p>
+              <p class="food-tag">
+                ${food.strCategory}
+              </p>
+            </div>
+          </div>
+        `;
+        });
+      }
+      else {
+        searchingMessage.style.display = 'none';
+
+        errorMessage.style.display = 'block';
+        errorMessage.textContent = `No recipes found for "${searchKeyword}". Try another search term!`;
+      }
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
